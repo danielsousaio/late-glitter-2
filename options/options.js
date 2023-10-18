1e56f23a4ff06374bc1cf9f10cf7f69edca33bf8
Daniel Sousa <daniel.s@crowdbotics.com>
2023-10-18 17:00:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-glitter-2",
+  url: "https://late-glitter-2.botics.co",
+  api: "https://late-glitter-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
